a974d33ad4d9a9a77fa6f080ce822e8ad9524490
bvmbo <wiktorsz2001@wp.pl>
2023-07-21 12:47:23 +0200

    model renders on reticle

--- js/app1.js
+++ js/app1.js
@@ -1,13 +1,17 @@
 import * as THREE from "three";
 import { ARButton } from "three/addons/webxr/ARButton.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-let camera, scene, renderer, controller, controls, reticle;
+
+let camera, scene, renderer, controller, reticle, hasLoaded;
+
 let obj = new THREE.Object3D();
 let isModel = false;
 let hitTestSource = null;
 let hitTestSourceRequested = false;
+
 init();
 animate();
+
 function init() {
 	const container = document.createElement("div");
 	document.body.append(container);
@@ -18,6 +22,7 @@ function init() {
 		0.01,
 		40
 	);
+
 	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 	renderer.setPixelRatio(window.devicePixelRatio);
 	renderer.setSize(window.innerWidth, window.innerHeight);
@@ -31,14 +36,38 @@ function init() {
 	document.body.appendChild(
 		ARButton.createButton(renderer, { requiredFeatures: ["hit-test"] })
 	);
+
 	// function adds an object to the scene after user's click
 	function onSelect() {
-		// if (!isModel) loadModel("chair");
-		// isModel = true;
-
 		if (reticle.visible && !isModel) {
-			loadModel("chair");
 			isModel = true;
+
+			let model = "chair";
+
+			let loader = new GLTFLoader().setPath("models/");
+			loader.load(
+				model + ".glb",
+				(glb) => {
+					obj = glb.scene;
+					// obj.scale.set(
+					// 	1.2 * glb.scene.scale.x,
+					// 	1.2 * glb.scene.scale.y,
+					// 	1.2 * glb.scene.scale.z
+					// );
+
+					console.log(obj.position);
+
+					// obj.position.set(0, 0, -0.6).applyMatrix4(controller.matrixWorld);
+					reticle.matrix.decompose(obj.position, obj.quaternion, obj.scale);
+					scene.add(obj);
+
+					hasLoaded = true;
+
+					console.log(obj.position);
+				},
+				onProgress,
+				onError
+			);
 
 			// const geometry2 = new THREE.CylinderGeometry(
 			// 	0.1,
@@ -50,8 +79,10 @@ function init() {
 			// 	color: 0xffffff * Math.random(),
 			// });
 			// const mesh2 = new THREE.Mesh(geometry2, material2);
+			// reticle.matrix.decompose(objPos, objQua, objScale);
 			// reticle.matrix.decompose(mesh2.position, mesh2.quaternion, mesh2.scale);
 			// mesh2.scale.y = Math.random() * 2 + 1;
+			// mesh2.position.set(0, 0, -0.6).applyMatrix4(controller.matrixWorld);
 			// scene.add(mesh2);
 		}
 	}
@@ -101,6 +132,7 @@ function rotateObject() {
 		obj.rotation.y += deltaX / 100;
 	}
 }
+
 function addReticleToScene() {
 	const geometry = new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2);
 	const material = new THREE.MeshBasicMaterial();
@@ -109,42 +141,25 @@ function addReticleToScene() {
 	reticle.visible = false;
 	scene.add(reticle);
 }
+
 function onProgress(xhr) {
 	console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
 }
+
 function onError(xhr) {
 	console.error(xhr);
 }
-function loadModel(model) {
-	let loader = new GLTFLoader().setPath("models/");
-	loader.load(
-		model + ".glb",
-		(glb) => {
-			obj = glb.scene;
-			// obj.scale.set(
-			// 	0.5 * glb.scene.scale.x,
-			// 	0.5 * glb.scene.scale.y,
-			// 	0.5 * glb.scene.scale.z
-			// );
 
-			obj.position.set(0, 0, -0.6).applyMatrix4(controller.matrixWorld);
-			// obj.quaternion.setFromRotationMatrix(controller.matrixWorld);
-			scene.add(obj);
-
-			console.log(obj.position);
-		},
-		onProgress,
-		onError
-	);
-}
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
 function animate() {
 	renderer.setAnimationLoop(render);
 }
+
 function render(timestamp, frame) {
 	if (frame) {
 		const referenceSpace = renderer.xr.getReferenceSpace();
@@ -180,41 +195,43 @@ function render(timestamp, frame) {
 
 let touchDown, touchX, touchY, deltaX, deltaY;
 
-obj.addEventListener(
-	"touchstart",
-	(e) => {
-		e.preventDefault();
-		touchDown = true;
-		touchX = e.touches[0].pageX;
-		touchY = e.touches[0].pageY;
-	},
-	false
-);
+if (hasLoaded) {
+	obj.addEventListener(
+		"touchstart",
+		(e) => {
+			e.preventDefault();
+			touchDown = true;
+			touchX = e.touches[0].pageX;
+			touchY = e.touches[0].pageY;
+		},
+		false
+	);
 
-obj.addEventListener(
-	"touchend",
-	(e) => {
-		e.preventDefault();
-		touchDown = false;
-	},
-	false
-);
+	obj.addEventListener(
+		"touchend",
+		(e) => {
+			e.preventDefault();
+			touchDown = false;
+		},
+		false
+	);
 
-obj.addEventListener(
-	"touchmove",
-	(e) => {
-		e.preventDefault();
+	obj.addEventListener(
+		"touchmove",
+		(e) => {
+			e.preventDefault();
 
-		if (!touchDown) {
-			return;
-		}
+			if (!touchDown) {
+				return;
+			}
 
-		deltaX = e.touches[0].pageX - touchX;
-		deltaY = e.touches[0].pageY - touchY;
-		touchX = e.touches[0].pageX;
-		touchY = e.touches[0].pageY;
+			deltaX = e.touches[0].pageX - touchX;
+			deltaY = e.touches[0].pageY - touchY;
+			touchX = e.touches[0].pageX;
+			touchY = e.touches[0].pageY;
 
-		rotateObject();
-	},
-	false
-);
+			rotateObject();
+		},
+		false
+	);
+}
